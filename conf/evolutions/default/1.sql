450d0feef665d2b9fb2798ca8bc66039f0182e10
Brian Clapper <bmc@clapper.org>
2012-09-25 10:22:11 -0400

    Switched to H2 database for now.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,24 +1,25 @@
 # --- !Ups
+
 create table users(
-    id serial primary key,
+    id identity primary key,
     username varchar(30) not null unique,
     encrypted_password varchar(255) not null,
     is_admin boolean not null default false
 );
 
 create table sites(
-    id serial primary key,
+    id identity primary key,
     user_id integer not null,
     name varchar(255) not null,
     username varchar(255),
     email varchar(255),
     url varchar(255),
     password varchar(255),
-    notes text
+    notes varchar(4096)
 );
 
 create table site_tags(
-    id serial primary key,
+    id identity primary key,
     site_id integer not null,
     tag varchar(255) not null
 );
